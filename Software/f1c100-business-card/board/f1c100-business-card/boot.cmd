3bbda5a48ea49637e5979d98ddd903f13288af3a
Devin <devinatkin@gmail.com>
2024-12-11 15:56:10 -0700

    Revert "Update all the baud rates to 9600"
    This reverts commit 34b901bbc4fe49c5002befdfdd312f2b183c5494.

--- Software/f1c100-business-card/board/f1c100-business-card/boot.cmd
+++ Software/f1c100-business-card/board/f1c100-business-card/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,9600n8 rootwait root=/dev/mtdblock3 rw rootfstype=jffs2
+setenv bootargs console=ttyS0,115200n8 rootwait root=/dev/mtdblock3 rw rootfstype=jffs2
 sf probe 0
 sf read 0x80c00000 0x100000 0x10000
 sf read 0x80008000 0x120000 0x400000
