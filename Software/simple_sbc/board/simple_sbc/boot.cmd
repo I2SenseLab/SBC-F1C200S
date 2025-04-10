0ced37fd9b2b4b9d1b564573727093d3e9eaa44a
Santiago Fuentes <santigf12@gmail.com>
2025-04-09 22:59:10 -0600

    added new new boot partition

--- Software/simple_sbc/board/simple_sbc/boot.cmd
+++ Software/simple_sbc/board/simple_sbc/boot.cmd
@@ -1,5 +1,5 @@
 setenv bootargs "console=ttyS0,115200n8 root=/dev/mmcblk0p2 rootwait rw rootfstype=ext4"
 mmc dev 0
-ext4load mmc 0:2 0x80008000 /boot/zImage
-ext4load mmc 0:2 0x80C00000 /boot/f1c200s_simple_sbc.dtb
+fatload mmc 0:1 0x80008000 zImage
+fatload mmc 0:1 0x80C00000 f1c200s_simple_sbc.dtb
 bootz 0x80008000 - 0x80C00000
